7db8663c9653817b8853be85e40c635af201f062
Jhen Mu <terminal201467@gmail.com>
2023-08-17 22:01:56 +0800

    完成InputChecker的Input、Output

--- TaiwanArtion/UserFeature/InputChecker.swift
+++ TaiwanArtion/UserFeature/InputChecker.swift
@@ -6,13 +6,48 @@
 //
 
 import Foundation
+import RxSwift
+import RxRelay
+import RxCocoa
 
 protocol CheckerInput {
+    //1.帳號輸入
+    var accountInputRelay: BehaviorRelay<String> { get }
     
+    //2.密碼輸入
+    var passwordInputRelay: BehaviorRelay<String> { get }
+    
+    //5.驗證碼輸入
+    var verifiedInputRelay: BehaviorRelay<String> { get }
+    
+    //6.電子郵件輸入
+    var emailVerifiedInputRelay: BehaviorRelay<String> { get }
+    
+    //7.手機號碼輸入
+    var phoneVerifiedInputRelay: BehaviorRelay<String> { get }
 }
 
 protocol CheckerOutput {
+    //1.帳號輸入檢查
+    var accountCheckOutputSignal: Signal<Bool> { get }
     
+    //2.密碼輸入檢查
+    var passwordCheckOutputSignal: Signal<Bool> { get }
+    
+    //3.密碼提示檢查
+    var passwordHintOutputSignal: Signal<[String]> { get }
+    
+    //4.密碼強度檢查
+    var passwordStrenghtOutputSignal: Signal<String> { get }
+    
+    //5.驗證碼檢查
+    var verifyCodeCheckOutputSignal: Signal<String> { get }
+    
+    //6.電子郵件檢查
+    var emailCheckOutputSignal: Signal<[String]> { get }
+    
+    //7.手機號碼檢查
+    var phoneNumberOutoutSignal: Signal<[String]> { get }
 }
 
 protocol CheckInputOutputType {
@@ -22,18 +57,44 @@ protocol CheckInputOutputType {
     var output: CheckerOutput { get }
 }
 
-class InputChecker {
-    
+class InputChecker: CheckInputOutputType, CheckerInput, CheckerOutput {
+
     //MARK: -Input
     
+    var accountInputRelay: RxRelay.BehaviorRelay<String> = BehaviorRelay(value: "")
+    
+    var passwordInputRelay: RxRelay.BehaviorRelay<String> = BehaviorRelay(value: "")
+    
+    var verifiedInputRelay: RxRelay.BehaviorRelay<String> = BehaviorRelay(value: "")
+    
+    var emailVerifiedInputRelay: RxRelay.BehaviorRelay<String> = BehaviorRelay(value: "")
+    
+    var phoneVerifiedInputRelay: RxRelay.BehaviorRelay<String> = BehaviorRelay(value: "")
     
     //MARK: -Output
     
+    var accountCheckOutputSignal: RxCocoa.Signal<Bool> = Signal.just(false)
+    
+    var passwordCheckOutputSignal: RxCocoa.Signal<Bool> = Signal.just(false)
+    
+    var passwordHintOutputSignal: RxCocoa.Signal<[String]> = Signal.just([])
+    
+    var passwordStrenghtOutputSignal: RxCocoa.Signal<String> = Signal.just("")
+    
+    var verifyCodeCheckOutputSignal: RxCocoa.Signal<String> = Signal.just("")
+    
+    var emailCheckOutputSignal: RxCocoa.Signal<[String]> = Signal.just([])
+    
+    var phoneNumberOutoutSignal: RxCocoa.Signal<[String]> = Signal.just([])
     
     //MARK: -Logic Interface
     private let checkLogic = CheckLogicInterface()
     
     //MARK: -Input/Output
+    
+    var input: CheckerInput { self }
+    
+    var output: CheckerOutput { self }
     
     //訂閱
     init() {
